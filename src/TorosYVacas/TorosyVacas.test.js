6d150f8c85025d8647cd544e40e936ff88891dbd
Nicole Uribe <nikyaleuribe@hotmail.com>
2022-04-07 21:10:13 -0400

    3.Insertar varios caracteres(numeros y letras) y volverse una clave(string)

--- src/TorosYVacas/TorosyVacas.test.js
+++ src/TorosYVacas/TorosyVacas.test.js
@@ -9,4 +9,10 @@ describe("Parametros", () => {
     it("2.Insertar varios numeros y volverse una clave(string)", ()=>{
         expect(p.insertar(1234)).toEqual("1234");
     });
+
+    it("3.Insertar varios caracteres(numeros y letras) y volverse una clave(string)", ()=>{
+        expect(p.insertar("1ab4")).toEqual("1ab4");
+    });
+
+
 });
